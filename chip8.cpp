c07723c85bcd144c374ef59ce052040e841aeb68
Scott Hanbreg <shanberg@purdue.edu>
2016-07-06 23:34:07 -0400

    Done with skeleton code, will move to opcodes now

--- chip8.cpp
+++ chip8.cpp
@@ -62,6 +62,31 @@ chip8::void init() {
 chip8::bool drawFlag() {
 }
 chip8::void emulateCycle() {
+	// Fetch opcode
+	opcode = memory[pc] << 8 | memory[pc + 1];
+
+	// Decode opcode
+	switch (opcode & 0xF000) {
+		// some opcodes
+		case 0xA000: // ANNN: Sets I to the address NNN
+			// Execute opcode
+			I = opcode & 0x0FFF;
+			pc += 2;
+		break;
+
+		//more opcodes 
+
+		default:
+			printf("Unknown opcode: 0x%X\n", opcode);
+	}
+
+	// Update timers
+	if (delay_timer > 0) {
+		if (sound_timer == 1) {
+			printf("BEEP!\n");
+			sound_timer--;
+		}
+	}
 }
 chip8::void debugRender() {
 }
